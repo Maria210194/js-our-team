9f96110d99436a2549d01b3c874ace220ddf02c9
Maria Stachie <mary.stachie@gmail.com>
2022-03-21 15:24:34 +0100

    array of objects

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
 console.log('OK JS!!');
 
 // definire un array di oggetti che rappresentano i membri del team
-// ogni membro ha le informazioni necessarie per stampare la relativa card "Foto, Nome e Ruolo"
+// ogni membro ha le informazioni necessarie per stampare la relativa card  "Nome, Ruolo e Foto"
+
+
+// definire un array di oggetti che rappresentano i membri del team
+
+let team = [
+    {
+        'nome': 'Wayne Barnett',
+        'ruolo': 'Founder & CEO',
+        'foto': 'img'
+
+    },
+    {
+        'nome': 'Angela Caroll',
+        'ruolo': 'Chief Editor',
+        'foto': 'img'
+
+    },
+    {
+        'nome': 'Walter Gordon',
+        'ruolo': 'Office Manager',
+        'foto': 'img'
+
+    },
+    {
+        'nome': 'Angela Lopez',
+        'ruolo': 'Social Media Manager',
+        'foto': 'img'
+
+    },
+    {
+        'nome': 'Scott Estrada',
+        'ruolo': 'Developer',
+        'foto': 'img'
+
+    },
+    {
+        'nome': 'Barbara Ramos',
+        'ruolo': 'Graphic Designer',
+        'foto': 'img'
+
+    }
+
+];
+
+console.log(team);
+
+
+
+
+// ogni membro ha le informazioni necessarie per stampare la relativa card  "Nome, Ruolo e Foto"
