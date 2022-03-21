412462c367a5a8931ccc19bd5cdb18381948d2bd
Maria Stachie <mary.stachie@gmail.com>
2022-03-21 15:00:34 +0100

    Init

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log('OK JS!!');
+
+// definire un array di oggetti che rappresentano i membri del team
+// ogni membro ha le informazioni necessarie per stampare la relativa card "Foto, Nome e Ruolo"
